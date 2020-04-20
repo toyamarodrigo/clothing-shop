d8d021b9a147d2256125d58dacc8e398e22ce021
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2020-04-20 03:52:02 -0300

    added sign in view, form input + stying

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import FormInput from "../form-input/form-input.component";
+
+import "./sign-in.style.scss";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (e) => {
+    const { value, name } = e.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            onChange={this.handleChange}
+            value={this.state.email}
+            label="email"
+            required
+          />
+          <FormInput
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            label="password"
+            required
+          />
+
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
